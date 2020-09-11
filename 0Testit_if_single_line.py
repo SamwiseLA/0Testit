b0385e0d49c2f8cdcbfd0be1395c0078f4cb01be
SamwiseLA <github@samwiser.com>
2020-09-11 12:31:11 -0700

    Added an average function that loops until no input AND a single line [for loop]

--- 0Testit_if_single_line.py
+++ 0Testit_if_single_line.py
@@ -13,3 +13,16 @@ if n_1 * n_2 > 1000: s_oper = "+"
 n_val = n_1 * n_2 if n_1 * n_2 <= 1000 else n_1 + n_2
 
 pb(f"{n_1} {s_oper}\n{n_2} =\n----------\n{n_val}","If product of numbers is <= 1000, add them")
+
+l_nums = []
+s_input = "1"
+while s_input not in ["0", ""]  :
+    s_input = input("please give numbers to average (0/spc-ends): ")
+    if s_input not in ["0", ""]: l_nums.append(float(s_input))
+
+f_tot = 0
+if len(l_nums) > 0:
+    for f_num in l_nums: f_tot = f_tot + f_num
+
+    print(f"\n\n{l_nums}\n\nThe Average of {f_tot} / {len(l_nums)} = {f_tot / len(l_nums)}")
+
